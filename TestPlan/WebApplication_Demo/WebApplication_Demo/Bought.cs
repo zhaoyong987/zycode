6a89bb429a1294ce9dad5d9bafeb1692b559085f
2417598692 <2417598692@qq.com>
2021-02-07 14:12:47 +0800

    添加测试

---
+++ TestPlan/WebApplication_Demo/WebApplication_Demo/Bought.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Demo
+{
+    public class Bought
+    {
+        public User MadeBy { get; set; }
+        public bool IsBoughtBy(User user)
+        {
+            if (user.customer)
+                return true;
+            if (MadeBy == user)
+                return true;
+            return false;
+        }
+    }
+    public class User
+    { 
+        public bool customer { get; set; }
+    }
+}
---
+++ TestPlan/WebApplication_Demo/WebApplication_Demo/Test/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication_Demo.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        { 
+        
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void IsBoughtBy_UserIsCustomer_ReturnTrue()
+        {
+            var bought = new Bought();
+            var result = bought.IsBoughtBy(new User { customer = true });
+            Assert.IsTrue(result);
+        }
+    }
+}
